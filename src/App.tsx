4cee3a684e83adcb81531f20530a8b06a6a96690
Adithya-A51 <ee24b081@smail.iitm.ac.in>
2026-06-01 11:55:38 +0530

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
-function App(){
-    return <div><ListGroup></ListGroup></div>
-
+function App() {
+const [alert, setalert] = useState(false);
+  
+  return (
+    <div>
+      {alert && <Alert onClose={()=>setalert(false)}> This is an alert for stupid people.</Alert>}
+      <Button onClick={() =>{setalert(true);}} > 
+        Button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup() {
-    let items = ['New York', "London", "New Delhi", "Paris", "Bangalore"];
-    const handleClick = (event) => console.log(event);
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selected, setselected] = useState(-1);
+  //   const [name, setname] = useState("");
 
-    return (
-        <>
-            <h1> List </h1>
-            {items.length === 0 && <p>No Item Found</p>}
-            <ul className="list-group">
-                {items.map((item, index) => (<li className="list-group-item" key={item} onClick={handleClick}>  {item}</li>))}
-            </ul>
-        </>
-    );
-
+  return (
+    <>
+      <h1> {heading} </h1>
+      {items.length === 0 && <p>No Item Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selected === index ? "list-group-item active" : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setselected(index);
+              onSelectItem(item);
+            }}
+          >
+            {" "}
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
